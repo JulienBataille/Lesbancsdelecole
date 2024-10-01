52ddb80909461dc5443bb24684d9541014a4acdd
landesprit <julien.bataille40@gmail.com>
2024-10-01 13:35:36 +0200

    subcategory

---
+++ src/Entity/SubCategory.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Category;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\SubCategoryRepository;
+
+#[ORM\Entity(repositoryClass: SubCategoryRepository::class)]
+class SubCategory
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne(inversedBy: 'subCategories')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $category = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCategory(): ?category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?category $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+
+}
